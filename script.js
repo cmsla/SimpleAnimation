22def59a3c64edf423230d75cceb45115d6391d4
cmsla <moo2j@nate.com>
2014-09-12 16:22:16 +0900

    수정2

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 var game = new Phaser.Game(1920, 1080, Phaser.AUTO, '', {preload: preload, create: create});
+var parasol3
 
 function preload() {
-	game.load.image('parasol3', 'assets/parasol3.png');
+	game.load.spritesheet('parasol3', 'assets/parasol3.png', 300, 300);
 }
 
 function create() {
-	game.add.sprite(100, 100, 'parasol3');
+	parasol3= game.add.sprite(100, 100, 'parasol3');
+	parasol3.animations.add('walk');
+	parasol3.animations.play('walk', 15, true);
 }
