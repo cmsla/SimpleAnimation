b4b1b9ce948d12557fce7e661c7bc7a621f7b092
cmsla <moo2j@nate.com>
2014-09-12 16:27:54 +0900

    파라솔4

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 var game = new Phaser.Game(1920, 1080, Phaser.AUTO, '', {preload: preload, create: create});
-var parasol3
+var parasol4
 
 function preload() {
-	game.load.spritesheet('parasol3', 'assets/parasol3.png', 300, 300);
+	game.load.spritesheet('parasol3', 'assets/parasol4.png', 300, 300);
 }
 
 function create() {
-	parasol3= game.add.sprite(100, 100, 'parasol3');
-	parasol3.animations.add('walk');
-	parasol3.animations.play('walk', 15, true);
+	parasol4= game.add.sprite(100, 100, 'parasol3');
+	parasol4.animations.add('walk');
+	parasol4.inputEnabled = true;
+	parasol4.events.onInputDown.add(cowClick, this);
+}
+
+function cowClick() {
+  parasol4.animations.play('walk', 10, false);
 }
