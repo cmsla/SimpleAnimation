c474bb8b8d1a05007fb775831f6a92f7f8d62f7e
cmsla <moo2j@nate.com>
2014-09-12 15:45:53 +0900

    파라솔3 이미지 추가

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var game = new Phaser.Game(1920, 1080, Phaser.AUTO, '', {preload: preload, create: create});
 
 function preload() {
-
+	game.load.image('parasol3', 'assets/parasol3.png');
 }
 
 function create() {
-	
+	game.add.sprite(100, 100, 'parasol3');
 }
